5d181b0ae71ffcce820db90a4b81a8fe39638586
jakub <jakub.kmiecik96@gmail.com>
2022-12-08 01:05:36 +0100

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,24 +10,26 @@ const Home = () => {
         <section id='home'>
             <Header/>
             <Opening/>
-            <h2>Odnajdź klimat Azji Środkowej
-                w sercu Kotliny Kłodzkiej</h2>
-            <div className='home_text'>
-                <p>Azja Środkowa w sercu Kotliny Kłodzkiej? Brzmi nieprawdopodobnie, a jednak.<br/>
-                    <br/>
-                    Oryginalne, ręcznie wykonane, całoroczne jurty przywiezione z Bajkału zapewnią niepowtarzalny
-                    klimat Azji Środkowej w sercu Kotliny Kłodzkiej. Miejsce gdzie znajdziesz spokój, relaks i
-                    komfort. Poczujesz wolność stepów Mongolii i Kazachstanu, powiew wiatru wspaniałych gór
-                    Kirgizji. Wszystko to w sąsiedztwie niesamowitej Ziemi Kłodzkiej z jej kulturą, dziedzictwem i
-                    przyrodą.<br/>
-                    <br/>
-                    Zapraszamy Was więc do 4 jurt, z których w ciągu dnia roztacza się widok na Masyw Śnieżnika
-                    i Czarną Górę, a w nocy można podziwiać gwieździste niebo widoczne przez okno w kopule.
-                    Gwarantujemy
-                    niezapomniane wrażenia!
-                </p>
-            </div>
+            <div className='home_info'>
+                <h2>Odnajdź klimat Azji Środkowej
+                    w sercu Kotliny Kłodzkiej</h2>
+                <div className='home_text'>
+                    <p>Azja Środkowa w sercu Kotliny Kłodzkiej? Brzmi nieprawdopodobnie, a jednak.<br/>
+                        <br/>
+                        Oryginalne, ręcznie wykonane, całoroczne jurty przywiezione z Bajkału zapewnią niepowtarzalny
+                        klimat Azji Środkowej w sercu Kotliny Kłodzkiej. Miejsce gdzie znajdziesz spokój, relaks i
+                        komfort. Poczujesz wolność stepów Mongolii i Kazachstanu, powiew wiatru wspaniałych gór
+                        Kirgizji. Wszystko to w sąsiedztwie niesamowitej Ziemi Kłodzkiej z jej kulturą, dziedzictwem i
+                        przyrodą.<br/>
+                        <br/>
+                        Zapraszamy Was więc do 4 jurt, z których w ciągu dnia roztacza się widok na Masyw Śnieżnika
+                        i Czarną Górę, a w nocy można podziwiać gwieździste niebo widoczne przez okno w kopule.
+                        Gwarantujemy
+                        niezapomniane wrażenia!
+                    </p>
+                </div>
             <Video embedId="09_niRBvLPc"/>
+            </div>
             <Maps/>
             <Footer/>
         </section>
