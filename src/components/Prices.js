8101b9b56876d86cf942ba32d190b0008b146b67
jakub <jakub.kmiecik96@gmail.com>
2022-12-28 23:09:08 +0100

    yurts

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -8,30 +8,28 @@ const Prices = () => {
             <Header/>
             <section id='prices'>
                 <h1>Cennik</h1>
-                <p>Cennik
+                <p>
+                    Sezon niski – 450zl/doba<br/>
+                    Sezon wysoki - 500zł/doba<br/>
+                    Akceptujemy bony turystyczne<br/>
+                    <br/>
+                    Informacje<br/>
+                    • Doba hotelowa rozpoczyna się o godz. 15:00 w dniu przyjazdu<br/>
+                    • Pokoje należy opuścić do godziny 12:00 w dniu wyjazdu<br/>
+                    • Balia płatna 150zł za 3 godziny<br/>
+                    • Sauna płatna 150zł za 3 godziny<br/>
 
-                    Sezon niski – 450zl/doba
-                    Sezon wysoki - 500zł/doba
-                    Akceptujemy bony turystyczne
-                    Informacje
-                    • Doba hotelowa rozpoczyna się o godz. 15:00 w dniu przyjazdu
-                    • Pokoje należy opuścić do godziny 12:00 w dniu wyjazdu
-                    • Balia płatna 150zł za 3 godziny
-                    • Sauna płatna 150zł za 3 godziny
-
-                    Zadatek
-
-                    ZADATEK W WYSOKOŚCI 30% PLANOWANYCH KOSZTÓW POBYTU NALEŻY WPŁACIĆ PRZED PLANOWANYM PRZYJAZDEM NA:
-
-                    Domy Słońca Marek Kmiecik
-                    Jaszkowa Dolna 18 57-312
-                    Nr rachunku bankowego: …………………..
-
-                    Jeśli za pobyt ma zostać wystawiona faktura, prosimy o wpisanie takiej informacji w tytule przelewu
-                    z podaniem nazwy firmy oraz numerem NIP.
-
-
-                    ZRÓB TEŻ PODZIAŁY NA JURTY ZE ZDJĘCIAMI I OPISEM : SŁOŃCE, WIATR, OGIEŃ
+                    <h1>Zadatek</h1>
+                    <br/>
+                    ZADATEK W WYSOKOŚCI 30% PLANOWANYCH KOSZTÓW POBYTU NALEŻY WPŁACIĆ PRZED PLANOWANYM PRZYJAZDEM
+                    NA:<br/>
+                    <br/>
+                    Domy Słońca Marek Kmiecik<br/>
+                    Jaszkowa Dolna 18 57-312<br/>
+                    Nr rachunku bankowego: 54 1090 2385 0000 0001 4493 6234<br/>
+                    <br/>
+                    Jeśli za pobyt ma zostać wystawiona faktura, prosimy o wpisanie takiej
+                    informacji w tytule przelewu z podaniem nazwy firmy oraz numerem NIP.
                 </p>
             </section>
             <Footer/>
--- src/components/Yurts.js
+++ src/components/Yurts.js
@@ -39,52 +39,76 @@ const Yurts = () => {
                 <div className='yurts_info'>
                     <div id='sun'>
                         <h2>Słońce</h2>
-                        <p>Panuje w niej niesamowita jasność, właściwie niezależnie od pogody.<br/>
-                            Jurta stanowi jedno pomieszczenie o powierzchni 30m2 i wysokości 3,50m. Połączone łącznikiem
-                            z łazienką. Wyposażyliśmy ją w komplet mebli dobranych na wzór oryginalnych, ręcznie i
-                            samodzielnie malując nasze sosnowe meble, wykorzystując oryginalne ornamenty i
-                            kolorystykę.<br/>
-                            W połączeniu z elementami jurty malowanymi przez Buriatów powstała piękna całość.<br/>
-                            <br/>
-                            W założeniu jurta miała być ogrzewana tradycyjnie przy pomocy tzw. kozy burżujki opalanej
-                            drewnem. Jednak wizyta kominiarza i strażaka przekonały nas do zastosowania ogrzewania
-                            zgodnego z trendami i miejscem tj. elektrycznego. Jurtę wyposażyliśmy w
-                            system ogrzewania elektrycznego pozwalający na komfortowe zamieszkiwanie w niej przez cały
-                            rok.<br/>
-                            Szklana kopuła (TONO) zwieńczająca jurtę dopełnia efektu przebywania w świecie stepów Azji
-                            Środkowej – widać przez nią nocne niebo.</p>
+                        <div className='info_photo'>
+                            <p>
+                                Panuje w niej niesamowita jasność, właściwie niezależnie od pogody.<br/>
+                                Jurta stanowi jedno pomieszczenie o powierzchni 30m2 i wysokości 3,50m. Połączone
+                                łącznikiem
+                                z łazienką. Wyposażyliśmy ją w komplet mebli dobranych na wzór oryginalnych, ręcznie i
+                                samodzielnie malując nasze sosnowe meble, wykorzystując oryginalne ornamenty i
+                                kolorystykę.<br/>
+                                W połączeniu z elementami jurty malowanymi przez Buriatów powstała piękna całość.<br/>
+                                <br/>
+                                W założeniu jurta miała być ogrzewana tradycyjnie przy pomocy tzw. kozy burżujki
+                                opalanej
+                                drewnem. Jednak wizyta kominiarza i strażaka przekonały nas do zastosowania ogrzewania
+                                zgodnego z trendami i miejscem tj. elektrycznego. Jurtę wyposażyliśmy w
+                                system ogrzewania elektrycznego pozwalający na komfortowe zamieszkiwanie w niej przez
+                                cały
+                                rok.<br/>
+                                Szklana kopuła (TONO) zwieńczająca jurtę dopełnia efektu przebywania w świecie stepów
+                                Azji
+                                Środkowej – widać przez nią nocne niebo.
+                            </p>
+                            <CarouselYurts/>
+                        </div>
                     </div>
                     <div id='wind'>
                         <h2>Wiatr</h2>
-                        <p>Druga jurta stanęła tuż obok Jurty Słońce. Jej budowa to było nie lada wyzwanie i właściwie
-                            powinniśmy ją nazwać WICHER. Właśnie taka pogoda panowała w czasie jej montażu. Niemniej
-                            jednak udało się wspaniale. Mieliśmy już doświadczenie budowy, więc pomimo przeciwności
-                            zdołaliśmy ją postawić. Jej niesamowity kolor bardzo ładnie komponuje się z otoczeniem,
-                            zwłaszcza kwitnącym sadem i okolicznymi górami. Jej wykończenie oraz wyposażenie wskazuje na
-                            XXI wiek.... jesteśmy przecież w Europie.<br/>
-                            Przyjazna dla rodzin z dziećmi, ciepła (cały rok) przytulna, komfortowa, a jednocześnie
-                            autentyczna.</p>
+                        <div className='info_photo'>
+                            <CarouselYurts/>
+                            <p>
+                                Druga jurta stanęła tuż obok Jurty Słońce. Jej budowa to było nie lada wyzwanie i
+                                właściwie
+                                powinniśmy ją nazwać WICHER. Właśnie taka pogoda panowała w czasie jej montażu. Niemniej
+                                jednak udało się wspaniale. Mieliśmy już doświadczenie budowy, więc pomimo przeciwności
+                                zdołaliśmy ją postawić. Jej niesamowity kolor bardzo ładnie komponuje się z otoczeniem,
+                                zwłaszcza kwitnącym sadem i okolicznymi górami. Jej wykończenie oraz wyposażenie
+                                wskazuje na
+                                XXI wiek.... jesteśmy przecież w Europie.<br/>
+                                Przyjazna dla rodzin z dziećmi, ciepła (cały rok) przytulna, komfortowa, a jednocześnie
+                                autentyczna.
+                            </p>
+                        </div>
                     </div>
                     <div id='fire'>
                         <h2>Ogień</h2>
-                        <p>
-
-                        </p>
+                        <div className='info_photo'>
+                            <CarouselYurts/>
+                            <p>
+                                Jurta zrobiona w stylu boho, posiada dwa łoża małżeńskie, posiada łazienkę...
+                            </p>
+                        </div>
                     </div>
                     <div id='etno'>
                         <h2>Etno</h2>
-                        <p>
-                            Na ostatnią jurtę mieliśmy specjalny pomysł. Zupełnie inny od poprzednich. Udało się ją
-                            zbudować w trybie oryginalnym tj. w 3 dni od pierwszego ruchu ręki do zakończenia budowy.<br/>
-                            Brakuje jeszcze oryginalnego wyposażenia, które chcemy przywieźć z planowanej wyprawy do
-                            Azji Środkowej ale w obecnej sytuacji trudno coś planować konkretnie. Mamy jednak
-                            nadzieję....<br/>
-                            W środku - niesamowita przestrzeń do medytacji, jogi, różnego rodzaju zajęć tematycznych czy
-                            spotkań towarzyskich.<br/>
-                            Do dyspozycji znajdująca się nieopodal kuchnia i węzeł sanitarny (prysznic, WC, umywalka).
-                        </p>
+                        <div className='info_photo'>
+                            <p>
+                                Na ostatnią jurtę mieliśmy specjalny pomysł. Zupełnie inny od poprzednich. Udało się ją
+                                zbudować w trybie oryginalnym tj. w 3 dni od pierwszego ruchu ręki do zakończenia
+                                budowy.<br/>
+                                Brakuje jeszcze oryginalnego wyposażenia, które chcemy przywieźć z planowanej wyprawy do
+                                Azji Środkowej ale w obecnej sytuacji trudno coś planować konkretnie. Mamy jednak
+                                nadzieję....<br/>
+                                W środku - niesamowita przestrzeń do medytacji, jogi, różnego rodzaju zajęć tematycznych
+                                czy
+                                spotkań towarzyskich.<br/>
+                                Do dyspozycji znajdująca się nieopodal kuchnia i węzeł sanitarny (prysznic, WC,
+                                umywalka).
+                            </p>
+                            <CarouselYurts/>
+                        </div>
                     </div>
-                    <CarouselYurts/>
                 </div>
             </section>
             <Footer/>
