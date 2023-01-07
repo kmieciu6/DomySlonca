3edb2c21fc4e757c319296da22b90aca2c7dc4d6
jakub <jakub.kmiecik96@gmail.com>
2023-01-07 23:36:07 +0100

    opening

--- src/components/Opening.js
+++ src/components/Opening.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {MDBCarousel, MDBCarouselItem} from 'mdb-react-ui-kit';
+import Carousel from 'react-bootstrap/Carousel';
 import carousel1 from '../assets/carousel1.jpeg'
 import carousel2 from '../assets/carousel2.jpeg'
 import carousel3 from '../assets/carousel3.jpeg'
@@ -9,38 +9,43 @@ import carousel5 from '../assets/carousel5.jpeg'
 const Opening = () => {
     return (
         <section id='opening'>
-            <MDBCarousel className='carousel' showControls fade interval={3000}>
-                <MDBCarouselItem
-                    className='w-100 d-block'
-                    itemId={1}
-                    src={carousel1}
-                    alt="First slide"
-                />
-                <MDBCarouselItem
-                    className='w-100 d-block'
-                    itemId={2}
-                    src={carousel2}
-                    alt="Second slide"
-                />
-                <MDBCarouselItem
-                    className='w-100 d-block'
-                    itemId={3}
-                    src={carousel3}
-                    alt="Third slide"
-                />
-                <MDBCarouselItem
-                    className='w-100 d-block'
-                    itemId={4}
-                    src={carousel4}
-                    alt="Forth slide"
-                />
-                <MDBCarouselItem
-                    className='w-100 d-block'
-                    itemId={5}
-                    src={carousel5}
-                    alt="Fifth slide"
-                />
-            </MDBCarousel>
+            <Carousel fade className='carousel'>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block w-100"
+                        src={carousel1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block w-100"
+                        src={carousel2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block w-100"
+                        src={carousel3}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block w-100"
+                        src={carousel4}
+                        alt="Forth slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block w-100"
+                        src={carousel5}
+                        alt="Fifth slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
         </section>
     )
 }
