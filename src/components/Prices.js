5999e1e31a8f9d79cd2804eed26b11265d3a068d
jakub <jakub.kmiecik96@gmail.com>
2023-01-10 14:47:41 +0100

    prices

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "./header/Header";
 import Footer from "./Footer";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCalendarDays, faArrowRightLong} from "@fortawesome/free-solid-svg-icons";
+import {Link} from "react-router-dom";
 
 const Prices = () => {
     return (
@@ -8,41 +11,64 @@ const Prices = () => {
             <Header/>
             <section id='prices' className='pages'>
                 <h1>Cennik</h1>
+                <h3>Minimum 2 doby</h3>
                 <div className='price_text'>
-                    <h3>Minimum 2 doby</h3>
-                    <p>
-                        <br/>
-                        <b>Sezon wysoki - 500 zł/doba</b><br/>
-                        grudzień -> luty<br/>
-                        czerwiec -> wrzesień<br/>
-                        <br/>
-                        <b>Sezon niski – 450 zł/doba</b><br/>
-                        marzec -> maj<br/>
-                        październik -> listopad<br/>
-                        <br/>
+                    <div className='seasons price'>
+                        <p>
+                            <b>Sezon wysoki</b><br/>
+                            grudzień <FontAwesomeIcon icon={faArrowRightLong}/> luty<br/>
+                            czerwiec <FontAwesomeIcon icon={faArrowRightLong}/> wrzesień<br/>
+                        </p>
+                        <Link to='/reserve'><FontAwesomeIcon icon={faCalendarDays} className='calendar'/></Link>
+                        <h4>500 zł/doba</h4>
+                    </div>
+                    <span/>
+                    <div className='seasons price'>
+                        <p>
+                            <br/>
+                            <b>Sezon niski</b><br/>
+                            marzec <FontAwesomeIcon icon={faArrowRightLong}/> maj<br/>
+                            październik <FontAwesomeIcon icon={faArrowRightLong}/> listopad<br/>
+                        </p>
+                        <Link to='/reserve'><FontAwesomeIcon icon={faCalendarDays} className='calendar second'/></Link>
+                        <h4>450 zł/doba</h4>
+                    </div>
+                    <span/>
+                    <p className='bon'>
                         Akceptujemy bony turystyczne<br/>
-                        <br/>
+                    </p>
+                    <div className='information price'>
                         <h3>Informacje</h3>
-                        • Doba hotelowa rozpoczyna się o godz. 15:00 w dniu przyjazdu<br/>
-                        • Pokoje należy opuścić do godziny 12:00 w dniu wyjazdu<br/>
-                        • Balia płatna 150 zł<br/>
-                        • Sauna płatna 150 zł<br/>
-                        <br/>
-                        10% rabatu dla osób podróżujących motocyklami.<br/>
-                        <br/>
-                    </p>
-                    <h3>Zadatek</h3>
-                    <p>
-                        W celu potwierdzenia rezerwacji należy wpłacić zadatek w wysokość 30%<br/>
-                        <br/>
-                        Domy Słońca Marek Kmiecik<br/>
-                        Jaszkowa Dolna 18 57-312<br/>
-                        Nr rachunku bankowego: 54 1090 2385 0000 0001 4493 6234<br/>
-                        <br/>
-                        Jeśli za pobyt ma zostać wystawiona faktura, prosimy o poinformowaniu takiej informacji w tytule
-                        przelewu z podaniem nazwy firmy oraz numerem NIP.
-                        <br/>
-                    </p>
+                        <p>
+                            Doba hotelowa rozpoczyna się o godz. 15:00 w dniu przyjazdu<br/>
+                            Pokoje należy opuścić do godziny 12:00 w dniu wyjazdu<br/>
+                        </p>
+                    </div>
+                    <div className='additional price'>
+                        <h3>Dodatki</h3>
+                        <p>
+                            Balia płatna – 150 zł<br/>
+                            Sauna płatna – 150 zł<br/>
+                            Jazdy konne – do umówienia na miejscu lub przez telefon<br/>
+                            Przejażdżki motocyklowe – do umówienia na miejscu lub przez telefon<br/>
+                            <br/>
+                            10% rabatu dla osób podróżujących motocyklami.<br/>
+                        </p>
+                    </div>
+                    <div className='deposit price'>
+                        <h3>Zadatek</h3>
+                        <p>
+                            W celu potwierdzenia rezerwacji należy wpłacić zadatek w wysokość 30%<br/>
+                            <br/>
+                            Domy Słońca Marek Kmiecik<br/>
+                            Jaszkowa Dolna 18 57-312<br/>
+                            Nr rachunku bankowego: 54 1090 2385 0000 0001 4493 6234<br/>
+                            <br/>
+                            Jeśli za pobyt ma zostać wystawiona faktura, prosimy o poinformowaniu takiej informacji w
+                            tytule
+                            przelewu z podaniem nazwy firmy oraz numerem NIP.
+                        </p>
+                    </div>
                 </div>
             </section>
             <Footer/>
