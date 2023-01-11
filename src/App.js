0a88715c8d33312fd522a86160af57bb50295460
jakub <jakub.kmiecik96@gmail.com>
2023-01-11 15:39:23 +0100

    apps

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Prices from "./components/Prices";
 import Reserve from "./components/Reserve";
 import Contact from "./components/Contact";
 import PrivacyPolicy from "./components/PrivacyPolicy";
+import NotFoundPage from "./components/NotFoundPage";
 
 function App() {
     return (
@@ -15,12 +16,13 @@ function App() {
             <HashRouter>
                 <Routes>
                     <Route exact path='/' element={<Home/>}/>
-                    <Route path='/yurts' element={<Yurts/>}/>
-                    <Route path='/attractions' element={<Attractions/>}/>
-                    <Route path='/prices' element={<Prices/>}/>
-                    <Route path='/reserve' element={<Reserve/>}/>
-                    <Route path='/contact' element={<Contact/>}/>
-                    <Route path='privacy_policy' element={<PrivacyPolicy/>}/>
+                    <Route exact path='/yurts' element={<Yurts/>}/>
+                    <Route exact path='/attractions' element={<Attractions/>}/>
+                    <Route exact path='/prices' element={<Prices/>}/>
+                    <Route exact path='/reserve' element={<Reserve/>}/>
+                    <Route exact path='/contact' element={<Contact/>}/>
+                    <Route exact path='/privacy_policy' element={<PrivacyPolicy/>}/>
+                    <Route exact path='/**' element={<NotFoundPage/>}/>
                 </Routes>
             </HashRouter>
         </>
---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Container, Row, Col} from "react-bootstrap";
+
+const NotFoundPage = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Strona nie istnieje</h1>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default NotFoundPage;
