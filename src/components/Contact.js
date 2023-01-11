042c8967950c1f7f0ecc0be3db7ed650e038e93e
jakub <jakub.kmiecik96@gmail.com>
2023-01-11 13:50:52 +0100

    contact styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,17 +9,18 @@ const Contact = () => {
             <section id='contact' className='pages'>
                 <h1>Kontakt</h1>
                 <form>
-                    <label>
+                    <label className='label'>
                         <h5>Email</h5>
                         <input type='email' name='email' className='input' placeholder='twój@email.com'/>
                         <h5>Imię i nazwisko</h5>
                         <input type='name' name='name' className='input' placeholder='imię i nazwisko'/>
                         <h5>Tekst</h5>
                         <textarea type='text' name='text' className='input' placeholder='Treść wiadomości' rows={5}/>
-                        <div className='checkbox_agree'>
-                            <input type="checkbox" id="agree" name="agree"/>
-                            <label htmlFor="agree"><p>Wyrażam zgodę na przetwarzanie danych osobowych.</p></label>
-                        </div>
+                        <label className="checkbox_agree">
+                            <p>Wyrażam zgodę na przetwarzanie danych osobowych.</p>
+                            <input type="checkbox"/>
+                            <span className="checkmark"/>
+                        </label>
                     </label>
                     <input type="submit" className='button' value="Wyślij"/>
                 </form>
