f3f83056be2db6afe651d65f16a80a29c9fa11a6
jakub <jakub.kmiecik96@gmail.com>
2023-01-16 23:18:29 +0100

    form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,170 +1,153 @@
-import React, {Component} from "react";
+import React from 'react';
+import {ErrorMessage, Field, Formik} from 'formik';
+import {useForm} from '@formspree/react';
 import Header from "./header/Header";
 import Footer from "./Footer";
-import MyInput from "./MyInput";
-import MyTextArea from "./MyTextArea";
+import thanks from '../assets/thanks.jpeg'
+import {Link} from "react-router-dom";
 
-class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            name: '',
-            message: '',
-            agree: false,
-            errors: {
-                emailError: '',
-                nameError: '',
-                messageError: '',
-                agreeError: ''
-            }
-        }
-    }
+function Contact() {
+    let errEmail = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
 
-    changeHandler = event => {
-        let inputName = event.target.name;
-        let inputValue = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-        this.setState(prevState => ({
-            ...prevState, [inputName]: inputValue
-        }));
-    }
-
-    submissionHandler = event => {
-        let errorFound = false;
-        let errEmail = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-
-        if (!this.state.email || errEmail.test(this.state.email) === false) {
-            errorFound = true;
-            this.setState(prevState => ({
-                errors: {
-                    ...prevState.errors,
-                    emailError: 'Email nieprawidłowy'
-                }
-            }));
-        } else {
-            this.setState(prevState => ({
-                errors: {
-                    ...prevState.errors,
-                    emailError: ""
-                }
-            }))
-        }
-
-        if (this.state.name.length < 2) {
-            errorFound = true;
-            this.setState(prevState => ({
-                errors: {
-                    ...prevState.errors,
-                    nameError: 'Imię i nazwisko powinno mieć co najmniej 2 znaki'
-                }
-            }));
-        } else {
-            this.setState(prevState => ({
-                errors: {
-                    ...prevState.errors,
-                    nameError: ""
-                }
-            }))
-        }
-
-        if (this.state.message.length < 50) {
-            errorFound = true;
-            this.setState(prevState => ({
-                errors: {
-                    ...prevState.errors,
-                    messageError: 'Wiadomość powinna mieć co najmniej 50 znaków'
-                }
-            }));
-        } else {
-            this.setState(prevState => ({
-                errors: {
-                    ...prevState.errors,
-                    messageError: ""
-                }
-            }))
-        }
-
-        if (this.state.agree === false) {
-            errorFound = true;
-            this.setState(prevState => ({
-                errors: {
-                    ...prevState.errors,
-                    agreeError: 'Wymagana akceptacja'
-                }
-            }));
-        } else {
-            this.setState(prevState => ({
-                errors: {
-                    ...prevState.errors,
-                    agreeError: ''
-                }
-            }))
-        }
-
-        if (errorFound) {
-            event.preventDefault();
-        } else {
-            console.log(this.state);
-            event.preventDefault();
-        }
-    }
-
-    render() {
+    const [state, handleSubmit] = useForm("xgebvawj");
+    if (state.succeeded) {
         return (
             <>
                 <Header/>
                 <section id='contact' className='pages'>
-                    <h1>Kontakt</h1>
-                    <form onSubmit={this.submissionHandler}>
-                        <MyInput
-                            type='text'
-                            name='email'
-                            label='Email'
-                            className='input'
-                            placeholder='twój@email.com'
-                            value={this.state.email}
-                            onChange={this.changeHandler}
-                            error={this.state.errors.emailError}
-                        />
-                        <MyInput
-                            type='text'
-                            name='name'
-                            label='Imię i nazwisko'
-                            className='input'
-                            placeholder='Imię i nazwisko'
-                            value={this.state.name}
-                            onChange={this.changeHandler}
-                            error={this.state.errors.nameError}
-                        />
-                        <MyTextArea
-                            type='text'
-                            name='message'
-                            label='Wiadomość'
-                            className='input'
-                            value={this.state.message}
-                            onChange={this.changeHandler}
-                            error={this.state.errors.messageError}
-                        />
-                        <MyInput
-                            type='checkbox'
-                            name='agree'
-                            label='Wyrażam zgodę na przetwarzanie danych osobowych.'
-                            className='checkbox_agree'
-                            value={this.state.agree}
-                            onChange={this.changeHandler}
-                            error={this.state.errors.agreeError}
-                        />
-                        <MyInput
-                            type="submit"
-                            className='button'
-                            value="Wyślij"
-                        />
-                    </form>
+                    <div className='thanks'>
+                        <h1>Dziękujemy za kontakt</h1>
+                        <img src={thanks} alt=""/>
+                        <Link to='/'>
+                            <button className='button'
+                                    onClick={() => {
+                                        window.scroll({
+                                            top: 0,
+                                            left: 0,
+                                            behavior: "smooth",
+                                        });
+                                    }}>
+                                Wróć
+                            </button>
+                        </Link>
+                    </div>
                 </section>
                 <Footer/>
             </>
         )
     }
+    return (
+        <>
+            <Header/>
+            <section id='contact' className='pages'>
+                <h1>Kontakt</h1>
+                <Formik
+                    initialValues={{
+                        email: '',
+                        name: '',
+                        message: '',
+                        number: '',
+                        agree: false
+                    }}
+
+                    validate={values => {
+                        const errors = {};
+                        if (!values.email || errEmail.test(values.email) === false) {
+                            errors.email = 'Email nieprawidłowy';
+                        }
+                        if (values.name.length < 2) {
+                            errors.name = 'Imię i nazwisko powinno mieć co najmniej 2 znaki';
+                        }
+                        if (values.message.length < 50) {
+                            errors.message = 'Wiadomość powinna mieć co najmniej 50 znaków';
+                        }
+                        if (values.agree === false) {
+                            errors.agree = 'Wymagana akceptacja';
+                        }
+                        return errors;
+                    }}
+                    onSubmit={handleSubmit}
+                >
+                    {
+                        ({
+                             handleSubmit,
+                         }) => (
+                            <form onSubmit={handleSubmit}>
+                                <label className='label'>
+                                    <h5>Email</h5>
+                                    <Field
+                                        id="mail"
+                                        type="text"
+                                        name="email"
+                                        className='input'
+                                        placeholder='twój@email.com'
+                                    />
+                                    <ErrorMessage name="email" render={msg => <p className="alert">{msg}</p>}/>
+                                </label>
+
+                                <label className='label'>
+                                    <h5>Imię i nazwisko</h5>
+                                    <Field
+                                        id="name"
+                                        type="text"
+                                        name="name"
+                                        className='input'
+                                        placeholder='Imię i nazwisko'
+                                    />
+                                    <ErrorMessage name="name" render={msg => <p className="alert">{msg}</p>}/>
+                                </label>
+
+                                <label className='label'>
+                                    <h5>Numer telefonu (opcjonalne)</h5>
+                                    <Field
+                                        id="number"
+                                        type='number'
+                                        name='number'
+                                        className='input'
+                                        placeholder='Numer telefonu'
+                                    />
+                                </label>
+
+                                <label className='label'>
+                                    <h5>Wiadomość</h5>
+                                    <Field
+                                        as='textarea'
+                                        id="message"
+                                        type='text'
+                                        name='message'
+                                        className='input'
+                                        placeholder='Treść wiadomości'
+                                        rows={5}
+                                    />
+                                    <ErrorMessage name="message" render={msg => <p className="alert">{msg}</p>}/>
+                                </label>
+
+                                <label className='label'>
+                                    <Field
+                                        id="agree"
+                                        type='checkbox'
+                                        name='agree'
+                                        className='checkbox_agree'
+                                    />
+                                    <h5>Wyrażam zgodę na przetwarzanie danych osobowych.</h5>
+                                    <span className="checkmark"/>
+                                    <ErrorMessage name="agree" render={msg => <p className="alert alert_agree">{msg}</p>}/>
+                                </label>
+
+                                <button
+                                    type="submit"
+                                    disabled={state.submitting}
+                                    className='button'>
+                                    Wyślij
+                                </button>
+                            </form>
+                        )
+                    }
+                </Formik>
+            </section>
+            <Footer/>
+        </>
+    );
 }
 
 export default Contact;
