a014baa950cf93ef47d041ab2b77f429379df854
jakub <jakub.kmiecik96@gmail.com>
2023-01-18 12:42:05 +0100

    app

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route exact path='/' element={<Home/>}/>
-                    <Route exact path='/yurts' element={<Yurts/>}/>
-                    <Route exact path='/attractions' element={<Attractions/>}/>
-                    <Route exact path='/prices' element={<Prices/>}/>
-                    <Route exact path='/reserve' element={<Reserve/>}/>
-                    <Route exact path='/contact' element={<Contact/>}/>
-                    <Route exact path='/privacy_policy' element={<PrivacyPolicy/>}/>
-                    <Route path='/*' element={<NotFoundPage/>}/>
+                    <Route exact path='yurts' element={<Yurts/>}/>
+                    <Route exact path='attractions' element={<Attractions/>}/>
+                    <Route exact path='prices' element={<Prices/>}/>
+                    <Route exact path='reserve' element={<Reserve/>}/>
+                    <Route exact path='contact' element={<Contact/>}/>
+                    <Route exact path='privacy_policy' element={<PrivacyPolicy/>}/>
+                    <Route path='*' element={<NotFoundPage/>}/>
                 </Routes>
             </BrowserRouter>
         </>
