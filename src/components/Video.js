e2e5821252f88863b661a1fb955a2d833331dca9
jakub <jakub.kmiecik96@gmail.com>
2023-01-24 13:39:07 +0100

    videos

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,11 +4,11 @@ import PropTypes from "prop-types";
 const YoutubeEmbed = ({embedId}) => (
     <div className="video">
         <iframe
-            width="50%"
             src={`https://www.youtube.com/embed/${embedId}`}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             title='Budowa jurty'
+            className='video_screen'
         />
     </div>
 );
