7d41fd4d8ccfe680e82d0d42783cdcb29e379bd1
jakub <jakub.kmiecik96@gmail.com>
2023-01-25 12:53:02 +0100

    async image

--- src/components/Opening.js
+++ src/components/Opening.js
@@ -5,41 +5,42 @@ import carousel2 from '../assets/carousel2.jpeg'
 import carousel3 from '../assets/carousel3.jpeg'
 import carousel4 from '../assets/carousel4.jpeg'
 import carousel5 from '../assets/carousel5.jpeg'
+import AsyncImage from "./AsyncImage";
 
 const Opening = () => {
     return (
         <section id='opening'>
             <Carousel fade className='carousel'>
                 <Carousel.Item interval={3000}>
-                    <img
+                    <AsyncImage
                         className="d-block w-100"
                         src={carousel1}
                         alt="First slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item interval={3000}>
-                    <img
+                    <AsyncImage
                         className="d-block w-100"
                         src={carousel2}
                         alt="Second slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item interval={3000}>
-                    <img
+                    <AsyncImage
                         className="d-block w-100"
                         src={carousel3}
                         alt="Third slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item interval={3000}>
-                    <img
+                    <AsyncImage
                         className="d-block w-100"
                         src={carousel4}
                         alt="Forth slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item interval={3000}>
-                    <img
+                    <AsyncImage
                         className="d-block w-100"
                         src={carousel5}
                         alt="Fifth slide"
